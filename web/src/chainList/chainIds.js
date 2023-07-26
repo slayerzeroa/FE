7a8e1167c50f643727f611e4d297feb5ceea4d6d
slayerzeroa <slayerzeroa@gmail.com>
2023-07-26 15:16:00 +0900

    adding login event

---
+++ web/src/chainList/chainIds.js
@@ -0,0 +1,21 @@
+const chainIds = {
+    1: { name: "Ethereum mainnet", symbol: "ETH" },
+    3: { name: "Ropsten", symbol: "RopstenETH" },
+    4: { name: "Rinkeby", symbol: "RinkebyETH" },
+    5: { name: "Goerli", symbol: "GoerliETH" },
+    42: { name: "Kovan", symbol: "KovanETH" },
+    56: { name: "Binance Smart Chain Mainnet", symbol: "BNB" },
+    97: { name: "Binance Smart Chain Testnet", symbol: "tBNB" },
+    43114: { name: "Avalanche C-Chain", symbol: "AVAX" },
+    137: { name: "Polygon Mainnet", symbol: "MATIC" },
+    80001: { name: "Mumbai", symbol: "MATIC" },
+    42161: { name: "Arbitrum One", symbol: "ETH" },
+    10: { name: "Optimism", symbol: "ETH" },
+    250: { name: "Fantom Opera", symbol: "FTM" },
+    8217: { name: "Klaytn Mainnet Cypress", symbol: "KLAY" },
+    1001: { name: "baobob", symbol: "KLAY" },
+    61: { name: "Ethereum Classic Mainnet", symbol: "ETC" },
+    11155111: { name: "Sepolia", symbol: "SepoliaETH" },
+};
+
+export default chainIds;
--- web/src/pages/MainPage_Logout.js
+++ web/src/pages/MainPage_Logout.js
@@ -1,37 +1,122 @@
+/* eslint-disable */
+
 import styles from "./MainPage_Logout.module.css";
 import { NavLink } from "react-router-dom";
 import React from "react";
 
-const MainPage_Logout = () => {
+import { useState, useEffect, useCallback } from "react";
+import { ethers } from "ethers";
+import { BrowserProvider } from "ethers";
+import ConnectButton from "../components/ConnectButton";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+
+function MainPage_Logout() {
+    const [provider, setProvider] = useState(undefined);
+    const [signer, setSigner] = useState(undefined);
+    const [walletAddress, setWalletAddress] = useState(undefined);
+    const [currentBalance, setCurrentBalance] = useState(undefined);
+    const [chainId, setChainId] = useState(undefined);
+    const [isConnected, setIsConnected] = useState(false);
+
+    const navigate = useNavigate();
+
+    const connectWallet = useCallback(async () => {
+        try {
+            if (typeof window.ethereum !== "undefined") {
+                const _provider = await getProvider();
+                const _signer = await getSigner(_provider);
+                await getWalletData(_signer);
+            } else {
+                alert("please install MetaMask");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+
+    const getMetamaskData = async () => {
+        const _provider = await getProvider();
+        const _signer = await getSigner(_provider);
+        await getWalletData(_signer);
+    };
+
+    const getProvider = async () => {
+        const provider = await new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+
+        return provider;
+    };
+
+    const getSigner = async (provider) => {
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        setSigner(signer);
+
+        return signer;
+    };
+
+    const getWalletData = async (signer) => {
+        const result = await Promise.all([signer.getAddress(), signer.getBalance(), signer.getChainId()]);
+        setWalletAddress(result[0]);
+        setCurrentBalance(Number(ethers.utils.formatEther(result[1])));
+        setChainId(result[2]);
+    };
+
+    const notice = () => {
+        Swal.fire({
+            title: "로그인이 필요합니다.",
+            text: "로그인 버튼을 눌러 메타마스크 연동을 해주세요.",
+            icon: "warning",
+            confirmButtonText: "확인",
+        });
+    };
+
+    if (walletAddress === undefined) {
+        console.log("walletAddress is undefined");
+    } else {
+        console.log("walletAddress is defined");
+        navigate("/main", {
+            state: { walletAddress: walletAddress, currentBalance: currentBalance, chainId: chainId },
+        });
+    }
+
     return (
         <div className={styles.div}>
             <div className={styles.sidebar}>
-                <NavLink to="/main" style={{ textDecoration: "none" }}>
+                <NavLink to="/" style={{ textDecoration: "none" }}>
                     <div className={styles.logo}>
                         <img className={styles.icIcon} alt="" src="/ic.svg" />
                         <div className={styles.linkAjou}>Link Ajou</div>
                     </div>
                 </NavLink>
                 <div className={styles.nav} />
-                <div className={styles.compButton}>
+                {/* <ConnectButton
+                    className={styles.compButton}
+                    isConnected={isConnected}
+                    connectWallet={connectWallet}
+                    walletAddress={walletAddress}
+                    currentBalance={currentBalance}
+                    chainId={chainId}
+                /> */}
+                <button className={styles.compButton} onClick={() => connectWallet()}>
                     <b className={styles.text}>Connect</b>
-                </div>
+                </button>
+
+                <div>{walletAddress}</div>
+                <div>{currentBalance}</div>
             </div>
             <div className={styles.body}>
                 <div className={styles.body1}>
                     <img className={styles.bgIcon} alt="" src="/bg1@2x.png" />
                     <div className={styles.lH}>
-                        <NavLink to="/under0" style={{ textDecoration: "none", color: "black" }}>
-                            <div className={styles.compButton1}>
-                                <div className={styles.text1}>재학생</div>
-                            </div>
-                        </NavLink>
-                        <NavLink to="/grad0" style={{ textDecoration: "none", color: "black" }}>
-                            <div className={styles.compButton1}>
-                                <div className={styles.text1}>졸업생</div>
-                            </div>
-                        </NavLink>
-                        <div className={styles.compButton1}>
+                        <div className={styles.compButton1} onClick={notice}>
+                            <div className={styles.text1}>재학생</div>
+                        </div>
+                        <div className={styles.compButton1} onClick={notice}>
+                            <div className={styles.text1}>졸업생</div>
+                        </div>
+                        <div className={styles.compButton1} onClick={notice}>
                             <div className={styles.text1}>
                                 <p className={styles.p}>재학생/졸업생</p>
                                 <p className={styles.p}>인증하기</p>
@@ -46,6 +131,6 @@ const MainPage_Logout = () => {
             </div>
         </div>
     );
-};
+}
 
 export default MainPage_Logout;
