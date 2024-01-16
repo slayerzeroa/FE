aa9ae8b83c362424592c2e815155edfe7f903038
slayerzeroa <slayerzeroa@gmail.com>
2024-01-16 21:00:09 +0900

    YDM_240116

--- server/index.js
+++ server/index.js
@@ -12,13 +12,8 @@ const app = express();
 
 const fs = require("fs");
 
-fs.readFile("/volume1/docker/ajoufe/FE_ENV.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const db_env = data.split("\n");
-});
+const text = fs.readFileSync("/volume1/docker/ajoufe/FE_ENV.txt", "utf8");
+const db_env = text.split("\r\n");
 
 const db = mysql.createPool({
   host: db_env[0],
@@ -26,14 +21,6 @@ const db = mysql.createPool({
   password: db_env[2],
   database: db_env[3],
 });
-
-// // 로컬 db 설정
-// const db = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "1234",
-//   database: "test",
-// });
 
 app.use(
   cors({
@@ -54,64 +41,27 @@ app.listen(PORT, () => {
   console.log(`running on port ${PORT}`);
 });
 
-// // 게시판 코드
-// app.get("/list", (req, res) => {
-//   const sqlQuery = "SELECT * FROM BOARD;";
-//   db.query(sqlQuery, (err, result) => {
-//     res.send(result);
-//   });
-// });
+// 게시판 코드
+app.get("/list", (req, res) => {
+  const sqlQuery = "SELECT * FROM BOARD;";
+  db.query(sqlQuery, (err, result) => {
+    res.send(result);
+  });
+});
 
-// // GPA 코드 (교수님 NAS DB)
-// app.get("/gpa", (req, res) => {
-//   const sqlQuery =
-//     "SELECT FLOOR(gpa) AS rounded_gpa, COUNT(*) AS count FROM privacy GROUP BY rounded_gpa;";
-//   db.query(sqlQuery, (err, result) => {
-//     res.send(result);
-//   });
-// });
-
-// // Graduate 코드 (교수님 NAS DB)
-// app.get("/graduate", (req, res) => {
-//   const sqlQuery = "SELECT * FROM privacy;";
-//   db.query(sqlQuery, (err, result) => {
-//     res.send(result);
-//   });
-// });
-
-// // cors 허용해주는 코드 작성
-
-// let corsOptions = {
-//   origin: "*", // 출처 허용 옵션
-//   credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
-// };
-
-// // app.use(cors({ credentials: true, origin: "http://localhost:3000/qa" }));
-// app.use(cors(corsOptions));
-
-// GPA 코드
+// GPA 코드 (NAS DB)
 app.get("/gpa", (req, res) => {
   const sqlQuery =
-    "SELECT FLOOR(gpa) AS rounded_gpa, COUNT(*) AS count FROM GRADUATE GROUP BY rounded_gpa;";
+    "SELECT FLOOR(gpa) AS rounded_gpa, COUNT(*) AS count FROM privacy GROUP BY rounded_gpa;";
   db.query(sqlQuery, (err, result) => {
     res.send(result);
   });
 });
 
-// Graduate 코드
+// Graduate 코드 (NAS DB)
 app.get("/graduate", (req, res) => {
-  const sqlQuery = "SELECT * FROM GRADUATE;";
+  const sqlQuery = "SELECT * FROM privacy;";
   db.query(sqlQuery, (err, result) => {
     res.send(result);
-  });
-});
-
-// submit 데이터 저장 코드
-app.post("/submit", (req, res) => {
-  const data = req.body;
-  console.log(data);
-  const sqlQuery = "INSERT INTO requested (rowno) VALUES (1)";
-  db.query(sqlQuery, (err, result) => {
-    res.send("success!");
   });
 });
